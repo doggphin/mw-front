69df5ae19fdce29844a3daaaafca70f69313656a
Doggphin <doggphin@gmail.com>
2024-06-09 23:43:51 -0600

    Bad idea, switching back to localhost

--- src/lib/scripts/mtd-store.js
+++ src/lib/scripts/mtd-store.js
@@ -1,5 +1,5 @@
 import {writable} from 'svelte/store';
-export const BACKENDIP = "http://54.203.85.182:5173"
+export const BACKENDIP = "http://localhost:8000"
 export const PageNameStore = writable("");
 export const ProjectBoardStore = writable([]);
 export const ProjectDetailStore = writable();
